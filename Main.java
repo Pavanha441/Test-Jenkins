1aaf665ff4f1dec07a2d29826611d07eab0c1c2d
pavanha <pavan.alagur@cognissolutions.com>
2024-11-12 21:50:29 +0530

    added student llist

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main{
     public static void main (String[] args){
@@ -8,9 +10,18 @@ public class Main{
         System.err.println("Added one more file to jenkins");
 
         Student s =  new Student();
+         List<Student> list =   new ArrayList<>();
 
         s.sayHello();
-
+        for(int i = 0 ; i< 10;i++){
+            
+            list.add(new Student("Pavan H Alagur", "Class"+i));
+           }
+   
+   
+           for (Student student : list) {
+               System.out.println("StudentNAme -->" + student.getName()+"------ Class-->" + student.getClassName());
+           }
         
         
     }
--- Student.java
+++ Student.java
@@ -1,14 +1,35 @@
 public class Student {
 
-  //  private String name;
-    //private String className;
+    private String name;
+    private String className;
 
     public Student() {
         System.out.println("Creating a student object");
     }
 
-    public void sayHello(){
+    public Student(String name, String className) {
+        this.name = name;
+        this.className = className;
+    }
+
+    public void sayHello() {
         System.out.println("Hi Jenkins from students");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
